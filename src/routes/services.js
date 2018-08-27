3a91672c85cfbf6399c55808f34b76ed09601d83
richie <richard.itimi@interswitchgroup.com>
2018-08-27 08:03:32 +0100

    completed module5

--- src/routes/services.js
+++ src/routes/services.js
@@ -4,20 +4,20 @@ const router = express.Router();
 
 const { accounts, writeJSON } = require('../data');
 
-app.get('/transfer', (req, res) => {
+router.get('/transfer', (req, res) => {
   res.render('transfer');
 });
-app.post('/transfer', (req, res) => {
+router.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
   writeJSON();
   res.render('transfer', { message: 'Transfer Completed' });
 });
 
-app.get('/payment', (req, res) => {
+router.get('/payment', (req, res) => {
   res.render('payment', { account: accounts.credit });
 });
-app.post('/payment', (req, res) => {
+router.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
   writeJSON();
